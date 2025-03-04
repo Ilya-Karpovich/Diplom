221b2b33a4444cc1eb0ef17e85cf307fd10818ec
Илья <110288384+Ilya-Karpovich@users.noreply.github.com>
2025-03-04 20:07:58 +0300

    Add old diplom project and part of new diplom project

---
+++ DecanatClient/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecanatClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void titleBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void imageBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void descriptionBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void descriptionLB_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Server/Entity/News.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Entity
+{
+    [Index("id", IsUnique = true)]
+    public class News
+    {
+        [Column("id")]
+        [Required]
+        [Key]
+        public int id { get; set; }
+        [Column("title")]
+        [Required]
+        public string title {  get; set; }
+        [Column("image_sourse")]
+        public string newsImageSourse {  get; set; }
+        [Column("description")]
+        [Required]
+        public string description {  get; set; }
+
+    }
+}
